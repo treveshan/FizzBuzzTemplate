5a5d2f1fac39286e53dfe17a3dacfe015aba0f81
Treveshan Naidoo <treveshan.naidoo@gmail.com>
2023-03-23 15:55:57 +0200

    init

---
+++ DeveloperTest/FizzBuzz.cs
@@ -0,0 +1,10 @@
+﻿namespace DeveloperTest
+{
+    public class FizzBuzz
+    {
+        public string Calc(int number)
+        {
+         throw new NotImplementedException();
+        }
+    }
+}
---
+++ DeveloperTest/FizzBuzzTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace DeveloperTest
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GivenOne_ShouldReturnOne()
+        {
+            //Arrange
+            var fizzBuzz = new FizzBuzz();
+
+            //Act
+            var result = fizzBuzz.Calc(1);
+
+            //Assert
+            Assert.AreEqual("1", result);
+        }
+      
+    }
+}
